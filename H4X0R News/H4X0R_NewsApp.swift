d41d6946155d6b21ff2ac324002847a12983e4e0
Akhil Raj <akhil.gs@gmail.com>
2022-04-18 20:06:49 -0400

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Akhil Raj on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
